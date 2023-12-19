7bd4ef6a55aa1ef4f28e4b10f74abff1935957f8
Pablo Santos <pablo.santos.708@ufrn.edu.br>
2023-12-19 18:02:01 -0300

    feat: telas por status

--- src/main/java/com/mvc/mudi/repositorys/PedidoRepository.java
+++ src/main/java/com/mvc/mudi/repositorys/PedidoRepository.java
@@ -1,11 +1,15 @@
 package com.mvc.mudi.repositorys;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.mvc.mudi.models.Pedido;
+import com.mvc.mudi.models.StatusPedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long>{
+	List<Pedido> findByStatus(StatusPedido aguardando);
 
 }
